5f7c8345248469baab6f9de242cb976470c7c1f3
dadamu <p22626262@gmail.com>
2025-04-14 02:01:53 +0800

    feat: add ContractMessage struct and NewContractMessage function for contract handling

---
+++ internal/interfaces/contract.go
@@ -0,0 +1,24 @@
+package interfaces
+
+type ContractMessage struct {
+	Contract string
+	Method   string
+	Args     []byte
+	Sender   string
+}
+
+func NewContractMessage(contract, method string, args []byte, sender string) ContractMessage {
+	return ContractMessage{
+		Contract: contract,
+		Method:   method,
+		Args:     args,
+		Sender:   sender,
+	}
+}
+
+// ----------------------------------------------------------------------------
+
+type Transaction interface {
+	GetState() []byte
+	GetContractMessages() []ContractMessage
+}
